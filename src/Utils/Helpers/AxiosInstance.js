bc44ffd3ec14b8ed76c948a88af3dd5d61ce3363
Eva Fadhillah Ulia <115793590+evafadhillahulia@users.noreply.github.com>
2025-06-26 07:55:59 +0700

    Update AxiosInstance.js

--- src/Utils/Helpers/AxiosInstance.js
+++ src/Utils/Helpers/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://reactjsonserver-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
